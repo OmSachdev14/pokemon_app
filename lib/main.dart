8dc7b500c29f9e442b0ccadab7be7ff7052a9eb2
omsachdev <omsachdev14122005@gmail.com>
2025-09-10 10:30:39 +0530

    Included FireBase_Auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pokemon/screens/home_screen.dart';
 import 'package:pokemon/screens/login_screen.dart';
 
 void main() async {
@@ -13,6 +15,9 @@ class Pokedex extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirebaseAuth.instance != null ? HomeScreen() : LoginScreen(),
+    );
   }
 }
